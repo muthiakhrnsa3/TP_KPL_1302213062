5bfe14c786f6626e19a0d4fa17223098ac3ca9a7
Muthia Khairunnisa <127019755+muthiakhrnsa3@users.noreply.github.com>
2023-03-19 21:03:59 +0700

    Add files via upload

---
+++ tpmod3_1302213062/tpmod3_1302213062/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpmod3_1302213062
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            label1.Text = "Halo, " + textBox1.Text;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
