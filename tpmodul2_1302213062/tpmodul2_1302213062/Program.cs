663814f3947234f230ca023cd62a4c7c6e4f2b92
Muthia Khairunnisa <127019755+muthiakhrnsa3@users.noreply.github.com>
2023-03-19 21:02:16 +0700

    Add files via upload

---
+++ tpmodul2_1302213062/tpmodul2_1302213062/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Masukan satu karakter huruf : ");
+char input = Console.ReadKey().KeyChar;
+
+if (isVowel(input))
+{
+    Console.WriteLine($"\nHuruf {input} merupakan huruf vokal\n");
+}
+else
+{
+    Console.WriteLine($"\nHuruf {input} merupakan huruf konsonan\n");
+}
+
+bool isVowel(char input)
+{
+    char[] vowelList = { 'a', 'e', 'i', 'o', 'u', 'A', 'I', 'U', 'E', 'O' };
+
+    foreach (char vowel in vowelList)
+    {
+        if (vowel == input)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int[] evenList = new int[5];
+
+for (int i = 0, j = 2; i < 5; i++, j += 2)
+{
+    evenList[i] = j;
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Angka genap {i + 1} : {evenList[i]}");
+}
